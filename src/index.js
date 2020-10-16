be8c051a45178bd1852be258168aec0bb2d1e733
johnhcody <john.haner.cody@gmail.com>
2020-10-17 01:51:47 +0700

    one modal working

--- src/index.js
+++ src/index.js
@@ -181,34 +181,55 @@ function makeGraphArr(e) {
 }
 
 const modal = document.getElementById("myModal");
+const infoModal = document.getElementById("myModal2");
 
 // Get the button that opens the modal
 const btn = document.getElementById("myBtn");
+const infoBtn = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
+btn.onclick = function (event, modal) {
+    debugger
+    modal = document.getElementById("myModal");
+    openModal(event, modal);
+    // modal.style.display = "block";
+}
+
+infoBtn.onclick = function (event, infoModal) {
+    openModal(event, infoModal);
+    // modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+span.onclick = function (event, modal) {
+    debugger
+    document.getElementsByClassName('modal')
+    closeModal(event, modal);
+    //modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
+// could add a || statement, but I don't like that
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    debugger
+    if (event.target == modal || event.target == infoModal) {
+        event.target.style.display = "none";
     }
 }
 
-function openModal() {
-
+function openModal(event, modal) {
+    debugger
+    //event.currentTarget.style.display = "block";
+    modal.style.display = "block";
 }
 
 function closeModal() {
-
+    debugger
+    //document.getElementById(`${modal}`).style.display
+    //event.currentTarget.style.display = "none";
+    const modal = Object.values(document.getElementsByClassName('modal'));
+    modal.map(mod => {
+        return mod.style.display = "none";
+    })
 }
