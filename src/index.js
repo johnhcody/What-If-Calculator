82fa7c64f16eeeee0987f36195a95d155bc64cbc
johnhcody <john.haner.cody@gmail.com>
2020-10-16 22:39:06 +0700

    added logo

--- src/index.js
+++ src/index.js
@@ -26,25 +26,22 @@ if (reset) {
 
 }
 
-function resetGraph() {
-    // debugger
-    document.getElementById("int").innerHTML = '';
-    document.getElementById("no-int").innerHTML = '';
-    document.getElementById("input-form").reset();
-    let obj = {}
-    new Graph({
-        element: document.querySelector('#graph-wrapper'),
-        data: [obj],
-        totalWithInterest: 0
-    })
-    // debugger
-}
-
 
 if (submission) {
     submission.addEventListener('submit', makeGraphArr)
     submission.addEventListener('submit', selectQuote)
 }
+
+
+function resetGraph() {
+    document.getElementById("int").innerHTML = '';
+    document.getElementById("no-int").innerHTML = '';
+    document.getElementById("input-form").reset();
+    document.getElementById("graph-wrapper").innerHTML = '';
+}
+
+
+
 
 function selectQuote(e) {
     e.preventDefault();
@@ -193,5 +190,29 @@ function makeGraphArr(e) {
     })
 }
 
+// Get the modal
+const modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
    
